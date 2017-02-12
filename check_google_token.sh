ab56e0bd0d1574fe072fb6b13d2c2daa9191cece
Yiu Fat Tse <yiu@townsquaredigital.com>
2017-02-12 17:10:56 -0500

    added check_google_token.sh

---
+++ check_google_token.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+token=$(cat "${HOME}/.google_token_config" | awk -F= '/TOKEN/{print $2}')
+
+output=$(curl -s "https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=$token")
+scope=$(echo "${output}" | jq -r .scope)
+expires_in=$(echo "${output}" | jq -r .expires_in)
+
+if [ "${scope}" != "https://www.googleapis.com/auth/drive" ]; then
+        echo "There's an an issue with the validity of the token.  Please check the token in the cofig file ($HOME/.google_token_config)"
+        exit
+fi
+
+echo
+echo "Output:"
+echo "${output}"
+echo
+echo "Response: Token appears to be valid"
+echo
+echo "Token ${token}"
+echo
+echo "Time Left (seconds): ${expires_in}"
+echo

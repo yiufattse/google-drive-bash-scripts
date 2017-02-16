60357b06cb855a20bb95a6ee9a53b243cc8853b6
Yiu Fat Tse <yiu@townsquaredigital.com>
2017-02-16 00:10:29 -0500

    renamed variable

--- check_google_token.sh
+++ check_google_token.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-token=$(cat "${HOME}/.google_token_config" | awk -F= '/ACCESS_TOKEN/{print $2}')
+access_token=$(cat "${HOME}/.google_token_config" | awk -F= '/ACCESS_TOKEN/{print $2}')
 
-output=$(curl -s "https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=$token")
+output=$(curl -s "https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=$access_token")
 scope=$(echo "${output}" | jq -r .scope)
 expires_in=$(echo "${output}" | jq -r .expires_in)
 
 if [ "${scope}" != "https://www.googleapis.com/auth/drive" ]; then
-        echo "There's an an issue with the validity of the token.  Please check the token in the cofig file ($HOME/.google_token_config)"
+        echo "There's an an issue with the validity of the access token.  Please check the token in the cofig file ($HOME/.google_token_config)"
         exit
 fi
 
@@ -17,7 +17,7 @@ echo "${output}"
 echo
 echo "Response: Token appears to be valid"
 echo
-echo "Token ${token}"
+echo "Token ${access_token}"
 echo
 echo "Time Left (seconds): ${expires_in}"
 echo

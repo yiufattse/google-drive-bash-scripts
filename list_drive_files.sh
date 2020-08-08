f39bc75135c893dd382ba6c4d9bdd428bd3b92db
Yiu F Tse <yiufattse@gmail.com>
2020-08-08 14:50:44 -0400

    list_drive_files.sh added parent_id in list

--- list_drive_files.sh
+++ list_drive_files.sh
@@ -6,19 +6,20 @@ access_token=$(cat "${HOME}/.google_token_config" | awk -F= '/ACCESS_TOKEN/{prin
 
 items=$( curl -s "https://www.googleapis.com/drive/v2/files?access_token=$access_token" | jq .items )
 
-echo $items | jq -r '.[] | "\(.modifiedDate)%%%\(.id)%%%\(.labels.trashed)%%%\(.title)"' | while read line; do
+echo $items | jq -r '.[] | "\(.modifiedDate)%%%\(.parents[0].id)%%%\(.id)%%%\(.labels.trashed)%%%\(.title)"' | while read line; do
 	modifiedDate=$(echo $line | awk -F%%% '{print $1}')
-	id=$(echo $line | awk -F%%% '{print $2}')
-	trashed=$(echo $line | awk -F%%% '{print $3}')
-	title=$(echo $line | awk -F%%% '{print $4}')
+	parent_id=$(echo $line | awk -F%%% '{print $2}')
+	id=$(echo $line | awk -F%%% '{print $3}')
+	trashed=$(echo $line | awk -F%%% '{print $4}')
+	title=$(echo $line | awk -F%%% '{print $5}')
 
 	title_escaped=$( echo "$title" | sed -e 's|"|\\"|g' )
 
 	if [ ! -z "$param" ] && [ "$param"=="--json" ]; then
-		line_json="{\"modifiedDate\": \"$modifiedDate\", \"id\": \"$id\", \"trashed\": \"$trashed\", \"title\": \"$title_escaped\"}"
+		line_json="{\"modifiedDate\": \"$modifiedDate\", \"parent_id\": \"$parent_id\", \"id\": \"$id\", \"trashed\": \"$trashed\", \"title\": \"$title_escaped\"}"
 		echo "$line_json" | jq .
 	else
-		line_json="$modifiedDate\t$id\t$trashed\t$title_escaped"
+		line_json="$modifiedDate\t$parent_id\t$id\t$trashed\t$title_escaped"
 		echo -e "$line_json"
 	fi
 done
